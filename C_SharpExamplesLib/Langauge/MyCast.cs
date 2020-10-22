a4927d447ca792894d1ea6cf42d51c589f976dc5
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2020-10-22 13:37:28 +0200

    Type-Testing

--- C_SharpExamplesLib/Langauge/MyCast.cs
+++ C_SharpExamplesLib/Langauge/MyCast.cs
@@ -10,6 +10,32 @@ namespace C_Sharp
 {
 	public class MyCast
 	{
+		private class MyCastClass1
+        {
+
+        }
+
+		private class MyCastClass2
+		{
+
+		}
+
+		// #type-testing #cast #is #as
+		private static void DoCasts(object x)
+        {
+			try
+			{
+				MyCastClass2 t1 = (MyCastClass2)x;
+			}
+			catch (InvalidCastException ivc)
+			{
+				;
+			}
+			MyCastClass2 t2 = x as MyCastClass2;
+			bool t3 = (x is MyCastClass1 t4);
+			bool t5 = (x is MyCastClass2);
+
+		}
 		// #cast #int #long 
 		public static void Test()
 		{
@@ -26,6 +52,10 @@ namespace C_Sharp
 			{
 				;
 			}
+
+			MyCastClass1 myCastClass1 = new MyCastClass1();
+            DoCasts((object)myCastClass1);
+
 		}
 	}
 }
