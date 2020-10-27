d4b85fa5e64195545620755a20d27852610a71f7
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2020-10-27 12:09:15 +0100

    threads, dispatcher, invoke

--- C_SharpExamplesLib/Langauge/MyProcess.cs
+++ C_SharpExamplesLib/Langauge/MyProcess.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Threading;
 
@@ -13,12 +14,33 @@ namespace C_Sharp
 
 		private static void Method()
 		{
+			Thread.Sleep(100);
+			Console.WriteLine("Method in thread " + Thread.CurrentThread.ManagedThreadId); 
 			return;
 		}
 
+		static void Task1()
+		{
+			Console.WriteLine("Task 1 starting in thread " + Thread.CurrentThread.ManagedThreadId);
+			Thread.Sleep(2000);
+			Console.WriteLine("Task 1 ending");
+		}
+
+		static void Task2()
+		{
+			Console.WriteLine("Task 2 starting in thread " + Thread.CurrentThread.ManagedThreadId);
+			Thread.Sleep(1000);
+			Console.WriteLine("Task 2 ending");
+		}
+
+        // #Invoke #Parallel #Dispatcher
 		public static void Test()
 		{
 			Dispatcher.CurrentDispatcher.Invoke(new Action(()=> { MyProcess.Method(); }));
+			Console.WriteLine("After asynchronus start of method within thread " + Thread.CurrentThread.ManagedThreadId);
+
+			Parallel.Invoke(() => Task1(), () => Task2());
+			Console.WriteLine("Finished processing within thread " + Thread.CurrentThread.ManagedThreadId);
 		}
 	}
 }
