8b29290a7288e5df576bb2c14410476f6de05ec0
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2020-11-27 15:48:46 +0100

    .Net Core included

---
+++ CoreUnitTest/CoreTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpCore;
+
+namespace CoreUnitTest
+{
+    [TestClass]
+    public class CoreTest
+    {
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///  Gets or sets the test context which provides
+		///  information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get { return testContextInstance; }
+			set { testContextInstance = value; }
+		}
+
+		[TestMethod]
+		public void CSarp8_Operators()
+		{
+			CSharp8.Operators();
+		}
+	}
+}
---
+++ NetCoreUnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using CSharpCore;
+
+namespace NetCoreUnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CShar8_Operators()
+        {
+            CSharp8.Operators();
+        }
+    }
+}
