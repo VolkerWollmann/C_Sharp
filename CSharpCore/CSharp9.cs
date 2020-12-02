857645555a9b8bb6370515ede43e8317c7d3e4e1
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2020-12-02 15:37:52 +0100

    init accessor

--- CSharpCore/CSharp9.cs
+++ CSharpCore/CSharp9.cs
@@ -1,21 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace CSharpCore
 {
     public class CSharp9
     {
-        // #record
-        internal record Person( string FirstName, string LastName);
+        // #record #contentEqualtity #referentialEquality
+        internal record Person(string FirstName, string LastName);
 
         public static void TestRecord()
         {
             Person p = new Person("Heinz", "Müller");
+
             var (f, l) = p;
+            Assert.AreEqual(f, p.FirstName);
             Console.WriteLine($"FirstName: {f} LastName: {l}");
+
+            Person son = p with { LastName = "Müllerson" };
+            Assert.AreEqual(p.FirstName, son.FirstName);
+        }
+
+
+        // #property #init #accessor
+        internal class Point
+        {
+            public int X { get; init; }
+
+            public int Y { get; init; }
+        }
+        public static void TestInit()
+        {
+            Point m = new Point { X = 41, Y = 42 };
+            
+            //Will not compile
+            //m.X = 43;
         }
     }
 }
