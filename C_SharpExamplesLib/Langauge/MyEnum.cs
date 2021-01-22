a82753182dc936d08885452985b4834913f1eac4
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2021-01-22 12:12:18 +0100

    max of enum

--- C_SharpExamplesLib/Langauge/MyEnum.cs
+++ C_SharpExamplesLib/Langauge/MyEnum.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,14 +32,21 @@ namespace C_Sharp
 
 		public static void Enum_Test()
 		{
+			// #enum number of elements
 			int numberOfDogs = Enum.GetValues(typeof(Dog)).Length;
 
+			// #enum to #array
 			var allDogs = Enum.GetValues(typeof(Dog));
 			ShowAllElement<Dog>(allDogs);
 
+			// #enum to #ienumerable
 			var iEnumerableDogs = allDogs.Cast<Dog>();
 			var allDogs2 = iEnumerableDogs.ToArray();
 			ShowAllElement2<Dog>(allDogs2);
+
+			// #enum max element
+			Dog maxDog = Enum.GetValues(typeof(Dog)).Cast<Dog>().Max();
+			Assert.AreEqual(maxDog, Dog.Cerberus);
 
 			var allCats = Enum.GetValues(typeof(Cat));
 			ShowAllElement<Dog>(allCats);
