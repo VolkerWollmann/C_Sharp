477eea0c56a27eac04afa6d0faa51f599fb4e740
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2021-08-17 07:57:09 +0200

    ExtensionMethod

--- C_SharpExamplesLib/Language/MyExtensionMethod.cs
+++ C_SharpExamplesLib/Language/MyExtensionMethod.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace C_Sharp.Language
 {
 	class MyMath
 	{
-		internal int A { get; }
-		internal int B { get; }
+		internal int A { get; set; }
+		internal int B { get; set; }
+
+        internal MyMath IncA()
+        {
+            A = A + 1;
+            return this;
+        }
+
 		internal MyMath(int a, int b)
 		{
 			A = a;
@@ -21,11 +29,28 @@ namespace C_Sharp.Language
 			return m.A + m.B; 
 		}
 
+        static MyMath IncB(this MyMath m)
+        {
+            m.B = m.B + 1;
+            return m;
+        }
+
 		public static void Test()
 		{
 			MyMath m = new MyMath(3, 4);
-			int result = m.Add();
+			
+            int result = m.Add();
 			Assert.AreEqual(7, result);
+
+            MyMath resultA = m.IncA().IncA();
+            Assert.AreEqual(5, resultA.A);
+
+            MyMath resultB = m.IncB().IncB();
+            Assert.AreEqual(6, resultB.B);
+
+            MyMath DoubleIncA(MyMath math) => math.IncA().IncA();
+            MyMath resultC = DoubleIncA(m);
+            Assert.AreEqual(7, resultC.A);
 		}
 	}
 }
