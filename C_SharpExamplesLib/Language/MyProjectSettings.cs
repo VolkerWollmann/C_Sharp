b78381efdb2bb4ad3fe38071e825ad010fcbb0bd
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2021-12-08 11:18:00 +0100

    Project Settings

---
+++ C_SharpExamplesLib/Language/MyProjectSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using C_Sharp.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace C_Sharp.Language
+{
+    public class MyProjectSettings
+    {
+
+        public static void ReadSettings()
+        {
+            Settings settings = new Settings();
+
+            var result = (string)settings.ProjectSetting;
+            Assert.AreEqual( result, "ProjectSettingValue");
+            //var appSettings = ConfigurationManager.AppSettings["UnitTestSetting"];
+        }
+    }
+}
---
+++ UnitTest/EnvironmentUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C_Sharp.Language.Event;
+using C_Sharp.Language;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class EnvironmentUnitTest
+    {
+
+        [TestMethod]
+
+        public void ReadSettings()
+        {
+            MyProjectSettings.ReadSettings();
+        }
+    }
+}
