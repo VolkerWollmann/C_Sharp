e99fb38bcb7f52f7456bc53699baab845796b24d
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2022-01-22 10:17:52 +0100

    more yield

--- C_SharpExamplesLib/Language/MyYield.cs
+++ C_SharpExamplesLib/Language/MyYield.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace C_Sharp.Language
@@ -8,39 +10,87 @@ namespace C_Sharp.Language
     {
         private static IEnumerable<int> OneToThree()
         {
+            Console.WriteLine("1..3: 1");
             yield return 1;
+            Console.WriteLine("1..3: 2");
             yield return 2;
+            Console.WriteLine("1..3: 3");
             yield return 3;
         }
 
         private static IEnumerable<int> FourToSix()
         {
+            Console.WriteLine("4..6: 4");
             yield return 4;
+            Console.WriteLine("4..6: 5");
             yield return 5;
+            Console.WriteLine("4..6: 6");
             yield return 6;
         }
 
         private static IEnumerable<int> OneToSix()
         {
-            foreach (int i in OneToThree())
+            foreach (int i in OneToThree().Union(FourToSix()))
             {
-                yield return i;
-            }
-
-            foreach (int i in FourToSix())
-            {
+                Console.WriteLine("1..6:" + i);
                 yield return i;
             }
         }
 
-        public static void Test()
+        private static IEnumerable<int> OneToSixUnionOfLists()
+        {
+            foreach (int i in OneToThree().ToList().Union(FourToSix().ToList()))
+            {
+                Console.WriteLine("1..6 Union of Lists:"+ i);
+                yield return i;
+            }
+        }
+
+        private static IEnumerable<int> OneToSixUnionOfEnumerableToList()
+        {
+            foreach (int i in OneToThree().Union(FourToSix()).ToList())
+            {
+                Console.WriteLine("1..6 Union of Enumerables to List: Return " + i);
+                yield return i;
+            }
+        }
+
+        private static IEnumerable<int> OneToSixAsThreeLists()
+        {
+            int k=0;
+            List<int> all = OneToThree().Union(FourToSix()).ToList();
+
+            for (int i = 0; i <= 2; i++)
+            {
+                List<int> l = new List<int>() {all[2*i], all[(2*i)+1]};
+                foreach (int j in l)
+                {
+                    Console.WriteLine("1..6 as threeLists: List {0} Element {1} ", i, k++);
+                    yield return j;
+                }
+            }
+        }
+
+        private static void Test(IEnumerable<int> iEnumerable)
         {
             int i = 1;
-            foreach (int j in MyYield.OneToSix())
+            foreach (int j in iEnumerable)
             {
                 Assert.AreEqual(i, j);
                 i++;
             }
+            Console.WriteLine("--");
         }
+
+
+        public static void Test()
+        { 
+            Test(OneToSix());
+            Test(OneToSixUnionOfLists());
+            Test(OneToSixUnionOfEnumerableToList());
+            Test(OneToSixAsThreeLists());
+        }
+
+
     }
 }
