964ef73a52e1ab071bcef670e9099163cb91c85f
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2023-01-30 19:52:05 +0100

    more implentation

--- C_SharpExamplesLib/Language/MyInheritance.cs
+++ C_SharpExamplesLib/Language/MyInheritance.cs
@@ -6,6 +6,7 @@ namespace C_Sharp.Language
     internal interface IInheritanceInterface
     {
         int GetValue();
+        int GetValue2();
     }
 
     internal class BaseClass : IInheritanceInterface
@@ -14,9 +15,14 @@ namespace C_Sharp.Language
         {
             return 1;
         }
+
+        public int GetValue2()
+        {
+            return 42;
+        }
     }
 
-    internal class DerivedClass : BaseClass, IInheritanceInterface
+    internal class DerivedClassWithPartialExplicit : BaseClass, IInheritanceInterface
     {
         // similar to #new 
         int IInheritanceInterface.GetValue()
@@ -25,7 +31,7 @@ namespace C_Sharp.Language
         }
     }
 
-    internal class DerivedClassWithExplictImplementation : BaseClass, IInheritanceInterface
+    internal class DerivedClassWithPartialNewImplementation : BaseClass, IInheritanceInterface
     {
         // #new
         public new int GetValue()
@@ -33,6 +39,51 @@ namespace C_Sharp.Language
             return 3;
         }
     }
+
+    internal class BaseClassWithExplicitImplementation : IInheritanceInterface
+    {
+        int IInheritanceInterface.GetValue()
+        {
+            return 4;
+        }
+
+        int IInheritanceInterface.GetValue2()
+        {
+            return 42;
+        }
+    }
+
+    internal class DerivedClassFromBaseClassWithExplicitImplementation : BaseClassWithExplicitImplementation, IInheritanceInterface
+    {
+        // similar to #new 
+        int IInheritanceInterface.GetValue()
+        {
+            return 5;
+        }
+
+        int IInheritanceInterface.GetValue2()
+        {
+            return 6;
+        }
+    }
+
+    internal class DerivedClassFromBaseClassWithPartialImplementation : BaseClassWithExplicitImplementation, IInheritanceInterface
+    {
+        // similar to #new 
+        int IInheritanceInterface.GetValue()
+        {
+            return 7;
+        }
+    }
+
+    //internal class DerivedClassFromBaseClassWithNewImplementation : BaseClassWithExplicitImplementation, IInheritanceInterface
+    //{
+    //    // will not compile
+    //    new int IInheritanceInterface.GetValue()
+    //    {
+    //        return 7;
+    //    }
+    //}
 
     public class MyInheritanceInterfaceTest
     {
@@ -42,16 +93,41 @@ namespace C_Sharp.Language
             int baseClassValue = baseClass.GetValue();
             Assert.AreEqual(1, baseClassValue);
 
-            DerivedClass derivedClass = new DerivedClass();
+            DerivedClassWithPartialExplicit derivedClass = new DerivedClassWithPartialExplicit();
             int derivedClassValue = derivedClass.GetValue();
             Assert.AreEqual(1, derivedClassValue);
 
             int derivedClassValueCasted = ((IInheritanceInterface) derivedClass).GetValue();
             Assert.AreEqual(2, derivedClassValueCasted);
 
-            DerivedClassWithExplictImplementation derivedClassWithExplictImpl = new DerivedClassWithExplictImplementation();
-            int derivedClassWithExplictImplValue = derivedClassWithExplictImpl.GetValue();
+            DerivedClassWithPartialNewImplementation derivedClassWithNewImpl = 
+                new DerivedClassWithPartialNewImplementation();
+            int derivedClassWithExplictImplValue = derivedClassWithNewImpl.GetValue();
             Assert.AreEqual(3, derivedClassWithExplictImplValue);
+
+            BaseClassWithExplicitImplementation baseClassWithExplicitImplementation =
+                new BaseClassWithExplicitImplementation();
+            // will not compile
+            //int baseClassWithExplicitImplementationValue = baseClassWithExplicitImplementation.GetValue();
+            int baseClassWithExplicitImplementationValue = 
+                ((IInheritanceInterface)baseClassWithExplicitImplementation).GetValue();
+            Assert.AreEqual(4, baseClassWithExplicitImplementationValue);
+
+            DerivedClassFromBaseClassWithExplicitImplementation derivedClassFromBaseClassWithExplicitImplementation 
+                = new DerivedClassFromBaseClassWithExplicitImplementation();
+            // will not compile
+            //int derivedClassFromBaseClassWithExplicitImplementationValue =
+            //    derivedClassFromBaseClassWithExplicitImplementation.GetValue();
+            int derivedClassFromBaseClassWithExplicitImplementationValue =
+                ((IInheritanceInterface)derivedClassFromBaseClassWithExplicitImplementation).GetValue();
+            Assert.AreEqual(5, derivedClassFromBaseClassWithExplicitImplementationValue);
+
+            DerivedClassFromBaseClassWithPartialImplementation derivedClassFromBaseClassWithPartialImplementation =
+                new DerivedClassFromBaseClassWithPartialImplementation();
+            int derivedClassFromBaseClassWithPartialImplementationValue =
+                ((IInheritanceInterface)derivedClassFromBaseClassWithPartialImplementation).GetValue();
+            Assert.AreEqual(7, derivedClassFromBaseClassWithPartialImplementationValue);
+
         }
     }
 }
