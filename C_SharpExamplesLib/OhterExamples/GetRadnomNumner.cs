32347f97cf06a5781ec284cd7f494cfe9f198ca3
Volker Ernst Wollmann <volkerwollmann@t-online.de>
2023-12-04 08:56:50 +0100

    Http Request

--- C_SharpExamplesLib/OhterExamples/GetRadnomNumner.cs
+++ C_SharpExamplesLib/OhterExamples/GetRadnomNumner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -13,6 +14,7 @@ namespace C_Sharp.OhterExamples
         private static int result = 0;
         private static async void DoTheRequest()
         {
+            result = 0;
             string url = "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new";
             using (HttpClient httpClient = new HttpClient())
             {
@@ -30,12 +32,47 @@ namespace C_Sharp.OhterExamples
                 finally { }
             }
         }
-        public static void Test()
+
+        private static async void DoRequest2()
+        {
+            result = 0;
+            string url = "https://jsonplaceholder.typicode.com/posts?userId=1";
+            //string jsonParameter =
+            //    @"userId=1";
+            using (HttpClient httpClient = new HttpClient())
+            {
+                try
+                {
+                    //var content = new StringContent(jsonParameter, Encoding.UTF8, "application/json");
+
+                    // Make a GET request
+                    HttpResponseMessage response = await httpClient.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseString = await response.Content.ReadAsStringAsync();
+                        
+                        Assert.IsTrue(responseString.Contains("\"id\": 1"));
+                        result = 1;
+                    }
+                }
+                finally { }
+            }
+        }
+
+        public static void TestHttpRequestSimple()
         {
             DoTheRequest();
             while (result == 0)
                 System.Threading.Thread.Sleep(1000);
             Console.WriteLine("Random number:" + result);
+        }
+
+        public static void TestHttpRequest2()
+        {
+            DoRequest2();
+            while (result == 0)
+                System.Threading.Thread.Sleep(1000);
         }
     }
 }
---
+++ UnitTest/OtherTests/HttpExamplesTest.cs
@@ -0,0 +1,21 @@
+﻿using C_Sharp.OhterExamples;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class HttpExamplesTest
+    {
+        [TestMethod]
+        public void HttpRequestSimple()
+        {
+            GetRadnomNumner.TestHttpRequestSimple();
+        }
+
+        [TestMethod]
+        public void HttpRequest2()
+        {
+            GetRadnomNumner.TestHttpRequest2();
+        }
+    }
+}
