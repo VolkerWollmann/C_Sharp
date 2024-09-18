521fdc9c04f2fc5510185ea7a6aebed8fb3d7fe4
Volker Wollmann <volkerwollmann@t-online.de>
2024-09-18 15:23:07 +0200

    Unit-Test 8.0

--- UnitTest/Properties/AssemblyInfo.cs
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -1,19 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("UnitTest")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTest")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("fb47993a-3f35-4755-b856-95de7c3cd80d")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
