2ece19860296c4c4f1bec8f3c47acad7ea2393a6
Volker Wollmann <volkerwollmann@t-online.de>
2025-02-12 11:22:45 +0100

    -

--- UnitTest/TaskUnitTest.cs
+++ UnitTest/TaskUnitTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using C_SharpExamplesLib.Language.Tasks;
-using System.Reflection;
 using C_SharpExamplesLib.Language.ConcurrentDataTypes;
 
 
