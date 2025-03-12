b9bfee7a7db80b954eb2af18907389d9080768a2
Volker Wollmann <volker@volker-wollmann.de>
2025-03-12 10:32:07 +0100

    Annotations Tests

---
+++ C_SharpExamplesLib/Language/DataAnnotation/MyDataAnnotations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace C_SharpExamplesLib.Language.DataAnnotations
+{
+    public class MyDataAnnotations
+    {
+        private class User
+        {
+            [Required(ErrorMessage = "Name is required.")]
+            [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters.")]
+            public string Name { get; set; }
+            [Required(ErrorMessage = "Email is required.")]
+            [EmailAddress(ErrorMessage = "Invalid email format.")]
+            public string Email { get; set; }
+            [Range(18, 100, ErrorMessage = "Age must be between 18 and 100.")]
+            public int Age { get; set; }
+        }
+
+        public static void Validate()
+        {
+            var user = new User
+            {
+                Name = "John Doe",
+                Email = "invalid-email", // Invalid email format
+                Age = 17 // Invalid age
+            };
+            var validationResults = new List<ValidationResult>();
+            var validationContext = new ValidationContext(user);
+            bool isValid = Validator.TryValidateObject(user, validationContext, validationResults, true);
+            
+            Assert.IsFalse(isValid);
+            Assert.IsTrue(validationResults.Any(e => e.ErrorMessage == "Invalid email format."));
+            Assert.IsTrue(validationResults.Any(e => e.MemberNames.Contains(nameof(User.Email))));
+            
+        }
+    }
+}
---
+++ UnitTest/AnnotationsTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using C_SharpExamplesLib.Language;
+using C_SharpExamplesLib.Language.DataAnnotations;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class AnnotationsTests
+    {
+        [TestMethod]
+        public void Validate()
+        {
+            MyDataAnnotations.Validate();
+        }
+        
+    }
+}
